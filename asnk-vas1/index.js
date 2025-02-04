e40252f81fc0c07af65c6b6f6095f9167c63542c
marshallovski <68496774+marshallovski@users.noreply.github.com>
2025-02-04 16:54:52 +0100

    [asnk-vas1]: whole code together

--- asnk-vas1/index.js
+++ asnk-vas1/index.js
@@ -1,8 +1,89 @@
-import Asnk from './asnk/index.js';
-import { cryptoRandomNumber } from "./utils/salt/cryptoRandomNumber.js";
+class Asnk {
+  constructor(version) {
+    this.version = version;
+  }
 
-import { $ } from '../utils/querySel.js';
+  async encrypt({ text, salt }) {
+    if (!salt) throw new Error('No `salt` provided!');
+    if (!text) throw new Error('No `text` provided!');
 
+    // in case when new version will come out
+    if (!this.version) throw new Error('No `version` provided (in class constructor)!');
+
+    let outputText = '';
+    const splittedText = text.split('');
+
+    let fails = 0;
+    let failedChars = [];
+
+    splittedText.forEach(token => {
+      let tableChar = token.charCodeAt(0);
+
+      if (tableChar) {
+        outputText += (tableChar * Number(salt)).toString() + ' '; // add a space to separate numbers
+      } else {
+        outputText += '? ';
+        fails++;
+        failedChars.push(token);
+      }
+    });
+
+    if (fails > 0) {
+      console.warn(`[asnk:${this.version}]: fails: ${fails}, failed to encrypt characters: ${failedChars.join(', ')}`);
+    }
+
+    return outputText.trim();
+  }
+
+  async decrypt({ text, salt }) {
+    if (!salt) throw new Error('No `salt` provided!');
+    if (!text) throw new Error('No `text` provided!');
+    if (!this.version) throw new Error('No `version` provided (in class constructor)!');
+
+    let outputText = '';
+    const splittedText = text.split(' ');
+
+    let fails = 0;
+    let failedChars = [];
+
+    splittedText.forEach(token => {
+      const num = parseFloat(token);
+      let originalChar;
+
+      if (!isNaN(num)) {
+        // divide by the salt
+        const charCode = Math.round(num / Number(salt));
+
+        // find the character
+        originalChar = String.fromCharCode(charCode);
+        outputText += originalChar;
+      } else {
+        outputText += '?';
+        fails++;
+        failedChars.push(token);
+      }
+    });
+
+    if (fails > 0) {
+      console.warn(`[asnk:${this.version}]: fails: ${fails}, failed to decrypt characters: ${failedChars.join(', ')}`);
+    }
+
+    return outputText;
+  }
+}
+
+export default Asnk;
+
+async function cryptoRandomNumber() {
+    const array = new Uint32Array(1);
+    crypto.getRandomValues(array);
+
+    return array[0];
+}
+
+function $(sel) {
+    return document.querySelector(sel);
+}
 
 const asnk = new Asnk('vas-1');
 
